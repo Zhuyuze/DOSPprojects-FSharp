1bd7e59e13d69f1c77ce24842e95cf0af8c8466a
Yuze Zhu <yuze.zhu@ufl.edu>
2021-12-11 01:59:35 -0500

    Tweeter client and server in F#

---
+++ tweeterClient/Program.fs
@@ -0,0 +1,228 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+open Akka
+open Akka.FSharp
+open Akka.Actor
+open Akka.Configuration
+open FSharp.Json
+
+
+type Message =
+    {
+        Content: string
+        Tag: string
+        User: int
+    }
+
+
+
+
+type requestMessage = 
+    {
+        Mode: int
+        UserName: string
+        UserID: int
+        SUserID: int
+        QueryTag: string
+        UserPassword: string
+        Tweet: string
+        Tag: string
+    }
+
+
+let r = System.Random()
+
+
+let generateRandomString (scope:int)=
+    let rv = r.Next() % scope
+    let l = if rv = 0 then 1 else rv
+    let mutable result = ""
+    for i in 0 .. l do
+        result <- result + char(r.Next() % 95 + 32).ToString()
+    result
+
+
+let configuration = 
+    ConfigurationFactory.ParseString(
+        @"akka {
+            actor {
+                provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+                deployment {
+                    /remoteecho {
+                        remote = ""akka.tcp://RemoteFSharp@127.0.0.1:34567""
+                    }
+                }
+            }
+            remote {
+                helios.tcp {
+                    port = 0
+                    hostname = ""127.0.0.1""
+                }
+            }
+        }")
+let system = ActorSystem.Create("RemoteFSharp", configuration)
+
+
+let clientActor (mailbox: Actor<_>) =
+    let mutable UserName = ""
+    let mutable UserID = -1
+    let mutable UserPassword = ""
+    let mutable server:ActorSelection = null
+    let mutable localMessage:list<Message> = []
+    let rec loop() = actor {
+        let! message = mailbox.Receive()
+        let sender = mailbox.Sender()
+        match box message with
+        | :? list<Message> as l ->
+            localMessage <- localMessage @ l
+            printfn "Client %A: New Message Received!" UserID
+        | :? int as a ->
+            UserID <- a
+            let request = {Mode = 5; UserName = UserName; UserID = UserID; SUserID = -1; QueryTag = ""; UserPassword = UserPassword; Tweet = ""; Tag = ""}
+            let json = Json.serialize request
+            server <! json
+            printfn "Registered with allocated ID: %A !" a
+        | :? ActorSelection as s ->
+            server <- s
+            printfn "Ready!"
+        | :? string as s ->
+            //printfn "Client %A: Start action: %A" UserID s
+            if s = "reg" then
+                if UserID <> -1 then
+                    printfn "Client %A: Already Registered!" UserID
+                else
+                    UserName <- (generateRandomString 50)
+                    UserPassword <- (generateRandomString 50)
+                    printfn "Registering with User Name: %A, Password: %A ..." UserName UserPassword
+                    let request = {Mode = 0; UserName = UserName; UserID = -1; SUserID = -1; QueryTag = ""; UserPassword = UserPassword; Tweet = ""; Tag = ""}
+                    let json = Json.serialize request
+                    server <! json
+            elif s = "twt" then
+                if UserID = -1 then
+                    printfn "Client %A: Not Registered!" UserID
+                else
+                    let c = generateRandomString 100
+                    let t = generateRandomString 5
+                    let request = {Mode = 1; UserName = UserName; UserID = UserID; SUserID = -1; QueryTag = ""; UserPassword = UserPassword; Tweet = c; Tag = t}
+                    printfn "Send a tweet with Tag %A: %A" t c
+                    let json = Json.serialize request
+                    server <! json
+            elif s = "rtt" then
+                if UserID = -1 then
+                    printfn "Client %A: Not Registered!" UserID
+                else
+                    let rv = r.Next()
+                    let request = {Mode = 1; UserName = UserName; UserID = UserID; SUserID = -1; QueryTag = ""; UserPassword = UserPassword; Tweet = localMessage.[rv % (localMessage.Length)].Content; Tag = localMessage.[rv % (localMessage.Length)].Tag}
+                    printfn "Send a retweet from user %A: %A" localMessage.[rv % (localMessage.Length)].User localMessage.[rv % (localMessage.Length)].Content
+                    let json = Json.serialize request
+                    server <! json
+            elif s = "sub" then
+                if UserID = -1 then
+                    printfn "Client %A: Not Registered!" UserID
+                else
+                    if localMessage.Length = 0 then
+                        printfn "Client %A: No Message!" UserID
+                    else
+                        let mutable subID = -1;
+                        while subID = -1 || subID = UserID do
+                            subID <- localMessage.[r.Next() % localMessage.Length].User
+                        let request = {Mode = 2; UserName = UserName; UserID = UserID; SUserID = subID; QueryTag = ""; UserPassword = UserPassword; Tweet = ""; Tag = ""}
+                        printfn "Subscribe User %A" subID
+                        let json = Json.serialize request
+                        server <! json
+            elif s = "req" then
+                if UserID = -1 then
+                    printfn "Client %A: Not Registered!" UserID
+                else
+                    let request = {Mode = 5; UserName = UserName; UserID = UserID; SUserID = -1; QueryTag = ""; UserPassword = UserPassword; Tweet = ""; Tag = ""}
+                    printfn "Requesting new messages..."
+                    let json = Json.serialize request
+                    server <! json
+            elif s = "rsb" then
+                if UserID = -1 then
+                    printfn "Client %A: Not Registered!" UserID
+                else
+                    let request = {Mode = 6; UserName = UserName; UserID = UserID; SUserID = -1; QueryTag = ""; UserPassword = UserPassword; Tweet = ""; Tag = ""}
+                    printfn "Requesting new messages from subscribed users..."
+                    let json = Json.serialize request
+                    server <! json
+            elif s = "rtg" then
+                if UserID = -1 then
+                    printfn "Client %A: Not Registered!" UserID
+                else
+                    let request = {Mode = 7; UserName = UserName; UserID = UserID; SUserID = -1; QueryTag = ""; UserPassword = UserPassword; Tweet = ""; Tag = "init"}
+                    printfn "Requesting new messages with Tag: init..."
+                    let json = Json.serialize request
+                    server <! json
+            elif s = "rqm" then
+                if UserID = -1 then
+                    printfn "Client %A: Not Registered!" UserID
+                else
+                    let request = {Mode = 8; UserName = UserName; UserID = UserID; SUserID = -1; QueryTag = ""; UserPassword = UserPassword; Tweet = ""; Tag = ""}
+                    printfn "Requesting all messages sent..."
+                    let json = Json.serialize request
+                    server <! json
+            else
+                let l = Json.deserialize<list<Message>> s
+                printfn "Client %A: New Messages Received!" UserID
+                for i in 0 .. (l.Length - 1) do
+                    printfn "Message %A from User %A with Tag %A: %A " (i+1) l.[i].User l.[i].Tag l.[i].Content
+                localMessage <- localMessage @ l
+                
+        | _ -> ()
+            
+        return! loop()
+    }
+    loop()
+
+
+[<EntryPoint>]
+let main argv =
+    
+    let addr = "akka.tcp://RemoteFSharp@127.0.0.1:34567/user/TweeterServer"
+    let server = system.ActorSelection(addr)
+    let client =
+        spawn system "client" clientActor
+    
+    client <! server
+    printfn "Press any key to register!"
+    Console.ReadLine() |> ignore
+
+    client <! "reg"
+
+    Console.ReadLine() |> ignore
+
+    let mutable flag = true
+    while flag do
+        printfn "Functions:"
+        printfn "0: quit"
+        printfn "1: send a tweet"
+        printfn "2: send a retweet"
+        printfn "3: subscribe a user"
+        printfn "4: request new messages"
+        printfn "5: request messages from subscription"
+        printfn "6: request messages with a tag"
+        printfn "7: request all messages of yourself"
+        
+        let input = Console.ReadLine()
+        if input = "0" then flag <- false
+        elif input = "1" then
+            client <! "twt"
+        elif input = "2" then
+            client <! "rtt"
+        elif input = "3" then
+            client <! "sub"
+        elif input = "4" then
+            client <! "req"
+        elif input = "5" then
+            client <! "rsb"
+        elif input = "6" then
+            client <! "rtg"
+        elif input = "7" then
+            client <! "rqm"
+        else printfn "wrong input"
+        System.Threading.Thread.Sleep(100)
+
+
+    0 // return an integer exit code
---
+++ tweeterServer/Program.fs
@@ -0,0 +1,193 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+open Akka
+open Akka.FSharp
+open Akka.Actor
+open Akka.Configuration
+open FSharp.Json
+
+
+type Message =
+    {
+        Content: string
+        Tag: string
+        User: int
+    }
+
+
+type UserInfo =
+    {
+        UserName: string
+        UserPassword: string
+        mutable UserMessage: list<int>
+        mutable UserSubscription: list<int>
+    }
+
+
+type requestMessage = 
+    {
+        Mode: int
+        UserName: string
+        UserID: int
+        SUserID: int
+        QueryTag: string
+        UserPassword: string
+        Tweet: string
+        Tag: string
+    }
+
+
+
+let config =
+    Configuration.parse // Change Server IP here
+        @"akka {
+            actor.provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+            remote.helios.tcp {
+                hostname = ""127.0.0.1""
+                port = 34567
+            }
+        }"
+let system = System.create "RemoteFSharp" config
+
+
+let r = System.Random()
+
+
+let serverActor (mailbox: Actor<_>) =
+    let mutable Users:list<UserInfo> = []
+    let mutable Messages:list<Message> = []
+    let rec loop() = actor {
+        let! message = mailbox.Receive()
+        let sender = mailbox.Sender()
+        match box message with
+        | :? requestMessage as rm ->
+            if rm.Mode = 0 then // register
+                Users <- {UserName = rm.UserName; UserPassword = rm.UserPassword; UserMessage = []; UserSubscription = []} :: Users
+                sender <! (Users.Length - 1) //allocate user id
+            elif rm.Mode = 1 then // tweet
+                Messages <- {Content = rm.Tweet; Tag = rm.Tweet; User = rm.UserID} :: Messages
+                Users.[rm.UserID].UserMessage <- (Messages.Length - 1) :: Users.[rm.UserID].UserMessage
+                if Messages.Length % 100 = 0 then
+                    printfn "[Server: Now we have %A users with %A messages!]" (Users.Length - 2) Messages.Length
+                    for i in 0 .. (Users.Length - 1) do
+                        printfn "[Server: User %A has %A messages and %A subscriptions.]" i Users.[i].UserMessage.Length Users.[i].UserSubscription.Length
+                    System.Threading.Thread.Sleep(50)
+            elif rm.Mode = 2 then // subscription
+                Users.[rm.UserID].UserSubscription <- rm.SUserID :: Users.[rm.UserID].UserSubscription
+            elif rm.Mode = 5 then // request 5 tweets || connected
+                if Messages.Length < 5 then
+                    let mutable returnList:list<Message> = []
+                    for i = 1 to Messages.Length do
+                        returnList <- Messages.[i-1] :: returnList
+                    sender <! returnList
+                else
+                    let mutable returnList:list<Message> = []
+                    for i in 1 .. 5 do
+                        returnList <- Messages.[r.Next() % Messages.Length] :: returnList
+                    sender <! returnList
+            elif rm.Mode = 6 then // request tweets from subscription
+                let mutable returnList:list<Message> = []
+                for i in 0 .. (Users.[rm.UserID].UserSubscription.Length - 1) do
+                    for j in 0 .. (Messages.Length - 1) do
+                        if Messages.[j].User = Users.[rm.UserID].UserSubscription.[i] then
+                            returnList <- Messages.[j] :: returnList
+                sender <! returnList
+            elif rm.Mode = 7 then // query tweets with Tag
+                let mutable returnList:list<Message> = []
+                for i in 0 .. Messages.Length - 1 do
+                    if Messages.[i].Tag = rm.Tag then
+                        returnList <- Messages.[i] :: returnList
+                sender <! returnList
+        | :? string as s -> 
+            if s = "init" then
+                Users <- {UserName = "User1"; UserPassword = "User1"; UserMessage = [1; 2; 3]; UserSubscription = [1]} :: Users
+                Users <- {UserName = "User2"; UserPassword = "User2"; UserMessage = [4; 5; 6; 7]; UserSubscription = [0]} :: Users
+                Messages <- {Content = "abcdefg"; Tag = "init"; User = 0} :: Messages
+                Messages <- {Content = "abcdefg"; Tag = "init"; User = 0} :: Messages
+                Messages <- {Content = "abcdefg"; Tag = "ini"; User = 0} :: Messages
+                Messages <- {Content = "abcdefg"; Tag = "init"; User = 1} :: Messages
+                Messages <- {Content = "abcdefg"; Tag = "ini"; User = 1} :: Messages
+                Messages <- {Content = "abcdefg"; Tag = "ini"; User = 1} :: Messages
+                Messages <- {Content = "abcdefg"; Tag = "init"; User = 1} :: Messages
+            else 
+                printfn "%A" s
+                let rm = Json.deserialize<requestMessage> s
+                if rm.Mode = 0 then // register
+                    Users <- Users @ [{UserName = rm.UserName; UserPassword = rm.UserPassword; UserMessage = []; UserSubscription = []}]
+                    printfn "A new user named %A registered!" rm.UserName
+                    sender <! (Users.Length - 1) //allocate user id
+                elif rm.Mode = 1 then // tweet
+                    Messages <- {Content = rm.Tweet; Tag = rm.Tag; User = rm.UserID} :: Messages
+                    Users.[rm.UserID].UserMessage <- (Messages.Length - 1) :: Users.[rm.UserID].UserMessage
+                    printfn "Message with Tag %A from User %A received!" rm.Tag rm.UserID
+                    System.Threading.Thread.Sleep(500)
+                    //if Messages.Length % 100 = 0 then
+                    printfn "[Server: Now we have %A users with %A messages!]" Users.Length Messages.Length
+                    for i in 0 .. (Users.Length - 1) do
+                        printfn "[Server: User %A has %A messages and %A subscriptions.]" i Users.[i].UserMessage.Length Users.[i].UserSubscription.Length
+                    
+                elif rm.Mode = 2 then // subscription
+                    Users.[rm.UserID].UserSubscription <- rm.SUserID :: Users.[rm.UserID].UserSubscription
+                    printfn "User %A has subscribed User %A" rm.UserID rm.SUserID
+                elif rm.Mode = 5 then // request 5 tweets || connected
+                    if Messages.Length < 5 then
+                        let mutable returnList:list<Message> = []
+                        for i = 1 to Messages.Length do
+                            returnList <- Messages.[i-1] :: returnList
+                        let json = Json.serialize returnList
+                        sender <! json
+                    else
+                        let mutable returnList:list<Message> = []
+                        for i in 1 .. 5 do
+                            returnList <- Messages.[r.Next() % Messages.Length] :: returnList
+                        let json = Json.serialize returnList
+                        printfn "Send 5 tweets to User %A" rm.UserID
+                        sender <! json
+                elif rm.Mode = 6 then // request tweets from subscription
+                    let mutable returnList:list<Message> = []
+                    for i in 0 .. (Users.[rm.UserID].UserSubscription.Length - 1) do
+                        for j in 0 .. (Messages.Length - 1) do
+                            if Messages.[j].User = Users.[rm.UserID].UserSubscription.[i] then
+                                returnList <- Messages.[j] :: returnList
+                    let json = Json.serialize returnList
+                    printfn "Send subscribed tweets to User %A" rm.UserID
+                    sender <! json
+                elif rm.Mode = 7 then // query tweets with Tag
+                    let mutable returnList:list<Message> = []
+                    for i in 0 .. Messages.Length - 1 do
+                        if Messages.[i].Tag = rm.Tag then
+                            returnList <- Messages.[i] :: returnList
+                    let json = Json.serialize returnList
+                    printfn "Send tweets with Tag %A to User %A" rm.Tag rm.UserID
+                    sender <! json
+                elif rm.Mode = 8 then // query tweets with Tag
+                    let mutable returnList:list<Message> = []
+                    for i in 0 .. Messages.Length - 1 do
+                        if Messages.[i].User = rm.UserID then
+                            returnList <- Messages.[i] :: returnList
+                    let json = Json.serialize returnList
+                    printfn "Send tweets of himself to User %A" rm.UserID
+                    sender <! json
+
+        | _ -> ()
+            
+        return! loop()
+    }
+    loop()
+
+
+[<EntryPoint>]
+let main argv =
+    
+
+    let server =
+        spawn system "TweeterServer" serverActor
+
+    server <! "init"
+
+
+    Console.ReadLine() |> ignore
+
+
+    0 // return an integer exit code
